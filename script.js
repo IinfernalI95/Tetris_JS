a947df0008bcf24350c4a55fbe487cc18d448671
Artur Lebedev <IinfernalI95@gmail.com>
2025-03-12 00:58:16 +0200

    Implement movement and update rendering logic

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const cellSize = 30;
-// Размер клетки
 const tetrominoSize = 30;
-
-// ctx.fillStyle = "blue";
-// ctx.fillRect(50, 50, 50, 50);
 
 const rows = 20;
 const colums = 10;
+
+const startX = 4;
+const startY = 0;
+
+let currentX = startX;
+let currentY = startY;
 
 // Определяем первую фигуру (O-тетромино)
 const O_Tetromino = [
@@ -17,7 +19,6 @@ const O_Tetromino = [
   [1, 1],
 ];
 
-// Отрисовка сетки
 function showGrid() {
   for (let row = 0; row < rows; row++) {
     for (let column = 0; column < colums; column++) {
@@ -27,14 +28,12 @@ function showGrid() {
   }
 }
 
-// Функция для рисования фигуры
 function drawTetromino(matrix, x, y, color) {
-  ctx.fillStyle = color; // Задаем цвет фигуры
+  ctx.fillStyle = color;
 
   for (let row = 0; row < matrix.length; row++) {
     for (let col = 0; col < matrix[row].length; col++) {
       if (matrix[row][col]) {
-        // Если в массиве 1 - рисуем клетку
         ctx.fillRect(
           (x + col) * tetrominoSize,
           (y + row) * tetrominoSize,
@@ -46,11 +45,17 @@ function drawTetromino(matrix, x, y, color) {
   }
 }
 
-// Координаты для первой фигуры (начальная позиция)
-const startX = 4; // Начало в 4-м столбце (по сетке)
-const startY = 0; // Начинаем с верхней части
-
 showGrid();
-
-// Вызываем функцию для рисования O-фигуры
 drawTetromino(O_Tetromino, startX, startY, "yellow");
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  showGrid(); // Перерисовываем сетку
+  drawTetromino(O_Tetromino, currentX, currentY, "yellow");
+
+  if (currentY < rows - O_Tetromino.length) {
+    currentY += 1;
+  }
+}
+
+setInterval(update, 500);
